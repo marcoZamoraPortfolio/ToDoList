1851b287afa8a8375235d3ec811531d3c8f45dcd
Marco Zamora <work.marcozamora@gmail.com>
2023-06-04 14:00:05 -0500

    * Added Fire base sdk clean state * added ruby 3.2.2

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Marco Zamora on 5/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
